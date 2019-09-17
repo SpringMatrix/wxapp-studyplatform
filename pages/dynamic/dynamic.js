08acb87db20a90321235fe418fb18d8fdf33d54b
Matrix <monoglo37@gmail.com>
2019-09-17 15:16:37 +0800

    首页错误修复，新增页面

--- pages/dynamic/dynamic.js
+++ pages/dynamic/dynamic.js
@@ -14,7 +14,6 @@ Page({
 
   },
   formatMsgTime: function(timespan) {
-
     var dateTime = new Date(timespan);
 
     var year = dateTime.getFullYear();
@@ -24,8 +23,7 @@ Page({
     var minute = dateTime.getMinutes();
     var second = dateTime.getSeconds();
     var now = new Date();
-    var now_new = Date.parse(now.toDateString());  //typescript转换写法
-
+    var now_new = Math.round(new Date()); //typescript转换写法
     var milliseconds = 0;
     var timeSpanStr;
 
@@ -33,17 +31,13 @@ Page({
 
     if (milliseconds <= 1000 * 60 * 1) {
       timeSpanStr = '刚刚';
-    }
-    else if (1000 * 60 * 1 < milliseconds && milliseconds <= 1000 * 60 * 60) {
+    } else if (1000 * 60 * 1 < milliseconds && milliseconds <= 1000 * 60 * 60) {
       timeSpanStr = Math.round((milliseconds / (1000 * 60))) + '分钟前';
-    }
-    else if (1000 * 60 * 60 * 1 < milliseconds && milliseconds <= 1000 * 60 * 60 * 24) {
+    } else if (1000 * 60 * 60 * 1 < milliseconds && milliseconds <= 1000 * 60 * 60 * 24) {
       timeSpanStr = Math.round(milliseconds / (1000 * 60 * 60)) + '小时前';
-    }
-    else if (1000 * 60 * 60 * 24 < milliseconds && milliseconds <= 1000 * 60 * 60 * 24 * 15) {
+    } else if (1000 * 60 * 60 * 24 < milliseconds && milliseconds <= 1000 * 60 * 60 * 24 * 15) {
       timeSpanStr = Math.round(milliseconds / (1000 * 60 * 60 * 24)) + '天前';
-    }
-    else if (milliseconds > 1000 * 60 * 60 * 24 * 15 && year == now.getFullYear()) {
+    } else if (milliseconds > 1000 * 60 * 60 * 24 * 15 && year == now.getFullYear()) {
       timeSpanStr = month + '-' + day + ' ' + hour + ':' + minute;
     } else {
       timeSpanStr = year + '-' + month + '-' + day + ' ' + hour + ':' + minute;
